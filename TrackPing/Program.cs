ab383658f583308ad07a152be512c03e2aab943e
Ryan <ryanwatson501@gmail.com>
2019-09-22 10:16:44 -0500

    Updated Help Examples
    Updated Help Examples

--- TrackPing/Program.cs
+++ TrackPing/Program.cs
@@ -14,11 +14,13 @@ namespace TrackPing
             if (options.Help)
             {
                 Console.WriteLine(parser.UsageInfo.ToString(78, false));
+                showExamples();
 
             }
             else if (parser.HasErrors)
             {
                 Console.WriteLine(parser.UsageInfo.ToString(78, true));
+                showExamples();
 
             }
             else if (options.Ip != null)
@@ -39,6 +41,29 @@ namespace TrackPing
                     pinger2.pinger(options.Ip, options.PingCount, options.PingSeconds, options.DatabaseName);
                 }
             }
+
+            void showExamples()
+            {
+                Console.WriteLine("Use Case:");
+                Console.WriteLine("Ping IP addresses and log the results to an SQLite Database.");
+                Console.WriteLine("Analyze the results to search for network latency and dropped packets.");
+                Console.WriteLine();
+                Console.WriteLine("Examining Results:");
+                Console.WriteLine("Use SQLiteBrowser \"https://sqlitebrowser.org\" to view, filter, and analyze the ping results.");
+                Console.WriteLine("Open Database, Click \"Browse Data\", Select the table, view/filter/analyze/export the data.");
+                Console.WriteLine();
+                Console.WriteLine("Examples:");
+                Console.WriteLine("TrackPing.exe -i 10.0.0.1");
+                Console.WriteLine("     --Pings 10.0.0.1 for the default number of pings at the default interval");
+                Console.WriteLine("TrackPing.exe -i 10.0.0.1 -i 10.0.0.2");
+                Console.WriteLine("     --Pings 10.0.0.1 & 10.0.0.2 for the default number of pings at the default interval");
+                Console.WriteLine("TrackPing.exe -i 10.0.0.1 -c 10 -s 2");
+                Console.WriteLine("     --Pings 10.0.0.1 for the 10 pings at a 2 second interval");
+                Console.WriteLine("TrackPing.exe -i 10.0.0.1 -d MyDatabase.sqlite");
+                Console.WriteLine("     --Pings Pings 10.0.0.1 for the default number of pings at the default interval, and sets the database name");
+            }
         }
+
+
     }
 }
